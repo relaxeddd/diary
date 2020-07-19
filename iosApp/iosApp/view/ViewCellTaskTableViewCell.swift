4319167c56f4f0a2a920013400b3d89fad29f50e
Chechin Vadim <vadim25091@yandex.ru>
2020-07-19 14:37:42 +0300

    [iOS] Task card implementation

---
+++ iosApp/iosApp/view/ViewCellTaskTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ViewCellTaskTableViewCell.swift
+//  iosApp
+//
+//  Created by Chechin Vadim on 18.07.2020.
+//  Copyright © 2020 Chechin Vadim. All rights reserved.
+//
+
+import UIKit
+
+class ViewCellTaskTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var textTitle: UILabel!
+    @IBOutlet weak var textDesc: UILabel!
+    
+}
