7779a007a3523fdea3557a771fea5b782e5fa5a4
Chechin Vadim <vadim25091@yandex.ru>
2020-09-13 10:13:47 +0300

    Refactored to its scripts

--- SharedCode/build.gradle.kts
+++ SharedCode/build.gradle.kts
@@ -10,6 +10,9 @@ plugins {
 //----------------------------------------------------------------------------------------------------------------------
 android {
     compileSdkVersion(29)
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    //sourceSets["main"].java.srcDirs("src/androidMain/kotlin")
+    //sourceSets["main"].res.srcDirs("src/androidMain/res")
     defaultConfig {
         minSdkVersion(21)
     }
@@ -18,14 +21,6 @@ android {
         getByName("release") {
             isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-
-    sourceSets {
-        getByName("main") {
-            manifest.srcFile(File("src/androidMain/AndroidManifest.xml"))
-            java.srcDirs("src/androidMain/kotlin")
-            res.srcDirs("src/androidMain/res")
         }
     }
 }
@@ -43,6 +38,14 @@ kotlin {
             }
         }
     }
+    /*android()
+    ios {
+        binaries {
+            framework {
+                baseName = "SharedCode"
+            }
+        }
+    }*/
 
     sourceSets {
         val versionCoroutines: String by project
@@ -82,26 +85,29 @@ sqldelight {
 
 //----------------------------------------------------------------------------------------------------------------------
 val packForXcode by tasks.creating(Sync::class) {
-    val targetDir = File(buildDir, "xcode-frameworks")
+    group = "build"
+
     val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
-    val framework = kotlin.targets
-        .getByName<KotlinNativeTarget>("ios")
-        .binaries.getFramework(mode)
+    //val sdkName = System.getenv("SDK_NAME") ?: "iphonesimulator"
+    //val targetName = "ios" + if (sdkName.startsWith("iphoneos")) "Arm64" else "X64"
+    //val framework = kotlin.targets.getByName<KotlinNativeTarget>(targetName).binaries.getFramework(mode)
+    val framework = kotlin.targets.getByName<KotlinNativeTarget>("ios").binaries.getFramework(mode)
 
     inputs.property("mode", mode)
     dependsOn(framework.linkTask)
 
+    val targetDir = File(buildDir, "xcode-frameworks")
     from({ framework.outputDirectory })
     into(targetDir)
 
     /// generate a helpful ./gradlew wrapper with embedded Java path
-    doLast {
+    /*doLast {
         val gradlew = File(targetDir, "gradlew")
         gradlew.writeText("#!/bin/bash\n"
                 + "export 'JAVA_HOME=${System.getProperty("java.home")}'\n"
                 + "cd '${rootProject.rootDir}'\n"
                 + "./gradlew \$@\n")
         gradlew.setExecutable(true)
-    }
+    }*/
 }
 tasks.getByName("build").dependsOn(packForXcode)
